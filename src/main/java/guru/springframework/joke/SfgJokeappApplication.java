4436f0750ae59905aebfa77406fb918d38f09f69
Kyle Schafer <kschay@protonmail.com>
2019-03-05 14:40:29 -0600

    Initial Maven project creation

---
+++ src/main/java/guru/springframework/joke/SfgJokeappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJokeappApplication.class, args);
+	}
+
+}
